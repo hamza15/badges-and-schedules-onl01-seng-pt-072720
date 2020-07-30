b2971ce786019edff63b7baeb350f2f581c735ce
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-07-30 05:03:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  list1 = []
+  names.each do |name|
+   list1.push("Hello, my name is #{name}.")
+  end
+  return list1
+end
+
+def assign_rooms(names)
+  list2 = []
+  names.each_with_index do |name, index|
+    final = index+1
+    list2.push("Hello, #{name}! You'll be assigned to room #{final}!")
+  end
+  return list2
+end
+
+def printer
+  batch_badge_creator.list2.each do |name|{puts "#{name}"}
+  assign_rooms.list1.each do |name|{puts "#{name}"}
+end
